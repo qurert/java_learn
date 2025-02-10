3b79e19ed2014487a5f646d38b2668ef9980220b
qurert <qurert1@gmail.com>
2025-02-10 17:31:49 +0600

    New classes and objects

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MFPJ.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MFPJ.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.sandbox;
 
+import org.w3c.dom.css.Rect;
+
 public class MFPJ {
 
   public static void main(String[] args) {
@@ -7,24 +9,15 @@ public class MFPJ {
     hello("user");
     hello("Alex");
 
-    double l = 5;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area (l));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area ());
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area (a, b));
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area ());
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
-  }
-
-  public static double area (double len) {
-    return len * len;
-  }
-
-  public static double area (double a, double b) {
-    return a * b;
   }
 
 }
